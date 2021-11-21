241af6cf316c36d56bdbf8fb211ebc998449e601
Doroshuk <dorosukolga100@gmail.com>
2021-11-21 15:56:11 +0200

    add sone npm and appBar,authNav

--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter, Switch } from 'react-router-dom';
-// import { PersistGate } from 'redux-persist/es/integration/react';
+import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/es/integration/react';
 import './index.css';
+import { store, persistor } from './redux/store';
 import 'react-toastify/dist/ReactToastify.css';
 import App from './App';
 // import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider>
-      {/* <PersistGate loading={null} persistor={}> */}
-      <BrowserRouter>
-        <Switch>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
           <App />
-        </Switch>
-      </BrowserRouter>
-      {/* </PersistGate> */}
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
